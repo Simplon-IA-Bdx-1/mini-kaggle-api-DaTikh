620bc3d2c31675782ee326cf91a8412a220b7dd6
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2020-10-06 11:00:12 +0200

    updated API

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test2.csv')
@@ -11,9 +11,9 @@ X_train = train.drop(columns=[label])
 X_test = test.drop(columns=[label]) 
 
 def main():
-    clf = SGDClassifier().fit(X_train, y_train)
-    df = pd.DataFrame(clf.predict(X_test))
-    df.to_csv('test2-predictions.csv', index=False)
+    clf = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
+    df = pd.DataFrame(clf.predict_proba(X_test))
+    df[1].to_csv('test2-predictions.csv', index=True)
 
 if __name__ == "__main__":
     main()
